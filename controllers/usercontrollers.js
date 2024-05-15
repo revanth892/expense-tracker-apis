b2fd17512312fac1a76d0cb6543ca8a98b3eaf4f
revanth892 <revanth9347569169@gmail.com>
2024-05-16 04:11:40 +0530

    added bcrypt to hash password during registering

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,15 +1,17 @@
 
 const User=require("../models/user.js")
-
+const bcrypt=require('bcrypt')
 const register=async(req,res)=>{
-    const {username ,password,email} =req.body.data;
+    let {username ,password,email} =req.body.data;
 
     try{
 
+        const hashed_password = await bcrypt.hash(password, 8)
+        password=hashed_password;
         const user= new User({
             username,email,password
         });
-
+        
         await user.save();
         res.status(201).json({message:"User created successfully"})
 
@@ -22,12 +24,17 @@ const register=async(req,res)=>{
 
 const login=async(req,res)=>{
     const {username ,password} =req.body.data;
-    console.log(1)
+    // console.log(1)
     try{
-        console.log(2)
-        const user =await User.find({username:username,password:password})
-        res.status(201).json({message:"User logged successfully",user:user})
-
+        // console.log(2)
+        if()
+        {
+            const user =await User.find({username:username,password:password})
+            res.status(201).json({message:"User logged successfully",user:user})
+        }
+        else{
+            
+        }
     }catch(err)
     {
         console.error("Error creating blog:", err);
